32fc20b15c58972f8f97fe07f96753fd5d311cc8
tijn-avontuur <bananenvla1234@gmail.com>
2025-02-14 11:41:18 +0100

    Userseeder
    Add userseeder with testuser

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'TestUser1',
+            'email' => 'testuser1@example.com',
+            'password' => '$2y$12$x6rtAKZw4lt9Tis4u8qrJe55GUVTq6TS25s2SdeRlgUOzBOoNg1MC',
+        ]);
+    }
+}
