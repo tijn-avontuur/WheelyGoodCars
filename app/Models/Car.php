ce4ed48019cc22b83f4b97e9d1976de594a036a6
tijn-avontuur <bananenvla1234@gmail.com>
2025-02-21 11:23:56 +0100

    New migrations & models
    Add migrations for tags and car tags, also models for tags and car tags.

---
+++ app/Models/Car.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'license_plate', 'brand', 'model', 'price', 'mileage', 
+        'seats', 'doors', 'production_year', 'weight', 'color', 'image', 
+        'sold_at', 'views'
+    ];
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'car_tag', 'car_id', 'tag_id');
+    }
+}
